a4da808eb0d90736ddcc62f2ed522d938563cc4d
hamzakassimi <42745180+hamzakassimi@users.noreply.github.com>
2018-11-22 12:54:23 +0000

    initial commit

---
+++ __manifest__.py
@@ -0,0 +1,36 @@
+# -*- encoding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Build It',
+    'version': '1.0',
+    'category': 'Purchases',
+    'author':'hka@novway.com',
+    'description': """
+Use Purchase Request module for requesting product.
+    """,
+    'summary': 'Construction site management',
+    'website': 'https://www.build-it.com',
+    'images': ['static/description/icon.jpg'],
+    'depends': ['mail'],
+    'data': [
+        'data/build_it_data.xml',
+        'security/build_it_security.xml',
+        'security/ir.model.access.csv',
+        'views/build_it_view.xml',
+        'views/project_project_views.xml',
+        'views/purchase_order_views.xml'
+    ],
+    'depends': [
+        'base',
+        'purchase',
+        'project',
+        'stock',
+        'hr',
+        ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'sequence': 105,
+    'license': 'AGPL-3',
+}
